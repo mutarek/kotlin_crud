9b980b7011046dd8a26e2f94a972e6a3406d3548
tarek <mohiuddintarek@gmail.com>
2023-03-21 17:22:41 +0600

    fragment added

---
+++ app/src/main/java/com/example/kotlin_crud/fragments/TodoView.kt
@@ -0,0 +1,25 @@
+package com.example.kotlin_crud.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlin_crud.R
+
+class TodoView : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo_view, container, false)
+    }
+
+}
